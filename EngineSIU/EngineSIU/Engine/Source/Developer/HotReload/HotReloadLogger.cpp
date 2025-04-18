f7fcf2bd215f4d5ee56fa3269fdfe8932618f1c5
gudtldn <gudtldn1234@gmail.com>
2025-04-18 22:39:03 +0900

    feat: HotReload 로깅 클래스 추가
    - FHotReloadLogger 클래스 구현 및 인터페이스(ICompilerLogger) 상속
    - LogError, LogWarning, LogInfo 함수 추가로 다양한 로그 레벨 처리 가능
    - HotReloadLogger.h/.cpp 파일 생성 및 프로젝트에 파일 등록

---
+++ EngineSIU/EngineSIU/Engine/Source/Developer/HotReload/HotReloadLogger.cpp
@@ -0,0 +1,27 @@
+﻿#include "HotReloadLogger.h"
+#include "UserInterface/Console.h"
+
+
+void FHotReloadLogger::LogError(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    UE_LOG(LogLevel::Error, format, args);
+    va_end(args);
+}
+
+void FHotReloadLogger::LogWarning(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    UE_LOG(LogLevel::Warning, format, args);
+    va_end(args);
+}
+
+void FHotReloadLogger::LogInfo(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    UE_LOG(LogLevel::Display, format, args);
+    va_end(args);
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Developer/HotReload/HotReloadLogger.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "ICompilerLogger.h"
+
+
+/**
+ * Hot Reload시 사용될 Logging 클래스 입니다.
+ */
+class FHotReloadLogger : public ICompilerLogger
+{
+public:
+    FHotReloadLogger() = default;
+
+    virtual void LogError(const char* format, ...) override;
+    virtual void LogWarning(const char* format, ...) override;
+    virtual void LogInfo(const char* format, ...) override;
+};
