3c66c1b9179faae118d9be4e544eb27b57153b68
gudtldn <gudtldn1234@gmail.com>
2025-04-18 22:09:26 +0900

    feat: HotReload 기능 추가를 위한 헤더 및 소스 파일 생성

---
+++ EngineSIU/EngineSIU/Engine/Source/Developer/HotReload/HotReload.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+
+class FHotReload
+{
+    FHotReload() = default;
+
+public:
+    static FHotReload& Get()
+    {
+        static FHotReload Instance;
+        return Instance;
+    }
+};
