932336c8941e67742ebdfa1ddd8a1deb3c4f09c9
gudtldn <gudtldn1234@gmail.com>
2025-04-23 00:10:11 +0900

    refactor: FShaderIncludeHandler의 메타데이터 주석 추가 및 GetIncludePaths 반환 타입 수정

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/D3D11RHI/DXDShaderManager.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/D3D11RHI/DXDShaderManager.cpp
@@ -6,7 +6,11 @@
 
 namespace fs = std::filesystem;
 
-
+/**
+ * HLSL 셰이더를 컴파일할 때 include 파일을 확인하는 데 사용됩니다.
+ * 
+ * 모든 포함된 파일 및 해당 확인 경로에 대한 메타데이터를 저장하여 추적 및 추가 처리를 수행합니다.
+ */
 class FShaderIncludeHandler : public ID3DInclude
 {
 public:
@@ -18,7 +22,7 @@ public:
     FShaderIncludeHandler(FShaderIncludeHandler&&) = delete;
     FShaderIncludeHandler& operator=(FShaderIncludeHandler&&) = delete;
 
-    [[nodiscard]] TArray<FFileMetadata> GetIncludePaths() const
+    [[nodiscard]] TArray<FFileMetadata>& GetIncludePaths()
     {
         return IncludePaths;
     }
