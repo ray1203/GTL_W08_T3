b3ba4c16af05e2b05066e6ee7a030247f45d77ef
jiwon1304 <143168061+jiwon1304@users.noreply.github.com>
2025-04-28 09:18:05 +0900

    misc : 주석 수정

--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Object.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Object.h
@@ -66,15 +66,15 @@ public:
 public:
     void* operator new(size_t size)
     {
-        //UE_LOG(ELogLevel::Display, "UObject Created : %d", size);
+        UE_LOG(ELogLevel::Display, "UObject Created : %d", size);
 
         void* RawMemory = FPlatformMemory::Malloc<EAT_Object>(size);
-        //UE_LOG(
-        //    ELogLevel::Display,
-        //    "TotalAllocationBytes : %d, TotalAllocationCount : %d",
-        //    FPlatformMemory::GetAllocationBytes<EAT_Object>(),
-        //    FPlatformMemory::GetAllocationCount<EAT_Object>()
-        //);
+        UE_LOG(
+            ELogLevel::Display,
+            "TotalAllocationBytes : %d, TotalAllocationCount : %d",
+            FPlatformMemory::GetAllocationBytes<EAT_Object>(),
+            FPlatformMemory::GetAllocationCount<EAT_Object>()
+        );
         return RawMemory;
     }
 
